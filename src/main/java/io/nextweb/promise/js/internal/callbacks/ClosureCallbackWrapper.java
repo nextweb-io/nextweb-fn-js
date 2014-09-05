3854c67677fee08ee216ba5dffc161185565bd5b
mxro <mxro@nowhere.com>
2014-09-05 16:50:20 +1000

    Deploying artifacts and documentation Autocommit Fri Sep 05 16:50:20 EST 2014

--- src/main/java/io/nextweb/promise/js/internal/callbacks/ClosureCallbackWrapper.java
+++ src/main/java/io/nextweb/promise/js/internal/callbacks/ClosureCallbackWrapper.java
@@ -6,34 +6,33 @@ import de.mxro.fn.Closure;
 
 public class ClosureCallbackWrapper {
 
-	private final Closure<Object> closure;
-	
+    private final Closure<Object> closure;
 
-	public JavaScriptObject getJavaScriptCallback() {
-		return createCallback();
-	}
+    public JavaScriptObject getJavaScriptCallback() {
+        return createCallback();
+    }
 
-	private void callCallback(Object param) {
-		closure.apply(param);
-	}
+    private void callCallback(final Object param) {
+        closure.apply(param);
+    }
 
-	private native JavaScriptObject createCallback()/*-{ 
-													
-													var self = this;
-													
-													var callbackFn = $entry(function(param) {
-													self.@io.nextweb.fn.js.internal.callbacks.ClosureCallbackWrapper::callCallback(Ljava/lang/Object;)(param);
-													});
-													
-													
-													return callbackFn;
-													
-													}-*/;
+    private native JavaScriptObject createCallback()/*-{ 
+                                                    
+                                                    var self = this;
+                                                    
+                                                    var callbackFn = $entry(function(param) {
+                                                    self.@io.nextweb.promise.js.internal.callbacks.ClosureCallbackWrapper::callCallback(Ljava/lang/Object;)(param);
+                                                    });
+                                                    
+                                                    
+                                                    return callbackFn;
+                                                    
+                                                    }-*/;
 
-	public ClosureCallbackWrapper(Closure<Object> closure) {
-		super();
-		this.closure = closure;
-		
-	}
+    public ClosureCallbackWrapper(final Closure<Object> closure) {
+        super();
+        this.closure = closure;
+
+    }
 
 }
