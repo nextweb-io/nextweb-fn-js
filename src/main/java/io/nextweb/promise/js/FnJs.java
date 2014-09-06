44262cd6c939af5b2817f11aaf22be70ecf652d8
mxro <mxro@nowhere.com>
2014-09-06 17:30:23 +1000

    Reworking callbacks from JavaScript back to Java/GWT; Fixing issue that persisted map is not started up correcty for js client Autocommit Sat Sep 06 17:30:23 EST 2014

--- src/main/java/io/nextweb/promise/js/FnJs.java
+++ src/main/java/io/nextweb/promise/js/FnJs.java
@@ -1,8 +1,6 @@
 package io.nextweb.promise.js;
 
-import io.nextweb.promise.js.callbacks.EmptyCallback;
 import io.nextweb.promise.js.internal.callbacks.ClosureCallbackWrapper;
-import io.nextweb.promise.js.internal.callbacks.EmptyCallbackWrapper;
 import io.nextweb.promise.js.types.JsArray;
 import io.nextweb.promise.js.wrapping.JsWrap;
 import io.nextweb.promise.js.wrapping.WrapperCollection;
@@ -15,65 +13,54 @@ import de.mxro.fn.Closure;
 
 public class FnJs {
 
-	public static final boolean isJsString(final Object value) {
-	    return value instanceof String;
-	}
+    public static final boolean isJsString(final Object value) {
+        return value instanceof String;
+    }
 
-	public static final boolean isJsInteger(final Object value) {
-	    return value instanceof Integer || value instanceof Short
-	            || value instanceof Long || value instanceof Byte;
-	}
+    public static final boolean isJsInteger(final Object value) {
+        return value instanceof Integer || value instanceof Short || value instanceof Long || value instanceof Byte;
+    }
 
-	public static final boolean isJsDouble(final Object value) {
-	    return value instanceof Float || value instanceof Double;
-	}
+    public static final boolean isJsDouble(final Object value) {
+        return value instanceof Float || value instanceof Double;
+    }
 
-	public static final boolean isJsBoolean(final Object value) {
-	    return value instanceof Boolean;
-	}
+    public static final boolean isJsBoolean(final Object value) {
+        return value instanceof Boolean;
+    }
 
-	public static final boolean isBasicJsType(final Object node) {
-	    return isJsString(node) || isJsInteger(node) || isJsDouble(node)
-	            || isJsBoolean(node);
-	}
+    public static final boolean isBasicJsType(final Object node) {
+        return isJsString(node) || isJsInteger(node) || isJsDouble(node) || isJsBoolean(node);
+    }
 
-	public static final native void triggerCallbackJs(JavaScriptObject fn,
-	JavaScriptObject jsArray)/*-{
-	                         fn.apply(this, jsArray.getArray());
-	                         }-*/;
+    public static final native void triggerCallbackJs(JavaScriptObject fn, JavaScriptObject jsArray)/*-{
+                                                                                                    fn.apply(this, jsArray.getArray());
+                                                                                                    }-*/;
 
-	public static final JsClosure asJsClosure(final JavaScriptObject fn,
-	        final WrapperCollection wrappers) {
-	
-	    return new JsClosure() {
-	
-	        @Override
-	        public void apply(final Object result) {
-	
-	            assert result instanceof JavaScriptObject;
-	
-	            triggerCallback(fn, wrappers, new Object[] { result });
-	
-	        }
-	    };
-	
-	}
+    public static final JsClosure asJsClosure(final JavaScriptObject fn, final WrapperCollection wrappers) {
 
-	public static final void triggerCallback(final JavaScriptObject fn,
-	        final WrapperCollection wrappers, final Object[] params) {
-	    triggerCallbackJs(fn, ExporterUtil.wrap(JsArray.wrap(JsWrap
-	            .toJsoArray(params, wrappers))));
-	}
-	
-	public static final JavaScriptObject exportCallback(EmptyCallback callback) {
-		EmptyCallbackWrapper wrapper = new EmptyCallbackWrapper(callback);
-		return wrapper.getJavaScriptCallback();
-		
-	}
-	
-	public static final JavaScriptObject exportCallback(Closure<Object> closure) {
-		ClosureCallbackWrapper wrapper = new ClosureCallbackWrapper(closure);
-		return wrapper.getJavaScriptCallback();
-	}
-	
+        return new JsClosure() {
+
+            @Override
+            public void apply(final Object result) {
+
+                assert result instanceof JavaScriptObject;
+
+                triggerCallback(fn, wrappers, new Object[] { result });
+
+            }
+        };
+
+    }
+
+    public static final void triggerCallback(final JavaScriptObject fn, final WrapperCollection wrappers,
+            final Object[] params) {
+        triggerCallbackJs(fn, ExporterUtil.wrap(JsArray.wrap(JsWrap.toJsoArray(params, wrappers))));
+    }
+
+    public static final JavaScriptObject exportCallback(final Closure<Object> closure) {
+        final ClosureCallbackWrapper wrapper = new ClosureCallbackWrapper(closure);
+        return wrapper.getJavaScriptCallback();
+    }
+
 }
