0380b4210f4afafb7a1fbdb541442fd2cf2acf3c
mxro <mxro@nowhere.com>
2014-09-06 17:26:23 +1000

    Reworking callbacks from JavaScript back to Java/GWT; Fixing issue that persisted map is not started up correcty for js client Autocommit Sat Sep 06 17:26:23 EST 2014

--- src/main/java/io/nextweb/promise/js/callbacks/JsStringValueCallbackWrapper.java
+++ src/main/java/io/nextweb/promise/js/callbacks/JsStringValueCallbackWrapper.java
@@ -1,6 +1,7 @@
 package io.nextweb.promise.js.callbacks;
 
 import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.ExporterUtil;
 import org.timepedia.exporter.client.NoExport;
 
 import com.google.gwt.core.client.JavaScriptObject;
@@ -20,7 +21,19 @@ public class JsStringValueCallbackWrapper {
 
     @Export
     public void onFailure(final JavaScriptObject t) {
+        final Object gwtInstance = ExporterUtil.gwtInstance(t);
 
+        if (gwtInstance instanceof Throwable) {
+            wrapped.onFailure((Throwable) gwtInstance);
+            return;
+        }
+
+        if (gwtInstance instanceof String) {
+            wrapped.onFailure(new Exception((String) gwtInstance));
+            return;
+        }
+
+        wrapped.onFailure(new Exception("Failure while processing native JavaScript function."));
     }
 
     public JsStringValueCallbackWrapper() {
