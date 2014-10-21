46ea570fd93d16b3124c6c09b786e385057c3f14
mxro <mxro@nowhere.com>
2014-10-21 13:30:04 +1100

    Fixing the way JavaScript exceptions are reported Autocommit Tue Oct 21 13:30:04 EST 2014

--- src/main/java/io/nextweb/promise/js/exceptions/ExceptionUtils.java
+++ src/main/java/io/nextweb/promise/js/exceptions/ExceptionUtils.java
@@ -8,136 +8,128 @@ import com.google.gwt.event.shared.UmbrellaException;
 
 public class ExceptionUtils {
 
-	public static final Throwable convertJavaScriptException(Object exception) {
-		if (exception instanceof Throwable) {
-			return (Throwable) exception;
-		}
+    public static final Throwable convertJavaScriptException(final Object exception) {
+        if (exception instanceof Throwable) {
+            return (Throwable) exception;
+        }
 
-		return new Exception("JavaScriptException: '" + exception.toString()
-				+ "' of class '" + exception.getClass() + "'");
-	}
+        return new Exception("JavaScriptException: '" + exception.toString() + "' of class '" + exception.getClass()
+                + "'");
+    }
 
-	public static final void triggerExceptionCallback(
-			final JavaScriptObject callback, final ExceptionResult r) {
-		triggerFailureCallbackJs(callback, r.origin().getClass().toString(),
-				unwrap(r.exception()).getMessage(),
-				getStacktrace(r.exception()), getOriginTrace(), getJsException(r.exception()));
-	}
+    public static final void triggerExceptionCallback(final JavaScriptObject callback, final ExceptionResult r) {
+        triggerFailureCallbackJs(callback, r.origin().getClass().toString(), unwrap(r.exception()).getMessage(),
+                getStacktrace(r.exception()), getOriginTrace(), getJsException(r.exception()));
+    }
 
-	public static final JavaScriptObject wrapExceptionResult(
-			final ExceptionResult r) {
+    public static final JavaScriptObject wrapExceptionResult(final ExceptionResult r) {
 
-		return (ExceptionUtils.wrapExceptionResult(r.origin().getClass()
-				.toString(), unwrap(r.exception()).getMessage(),
-				getStacktrace(r.exception()), getOriginTrace(), getJsException(r.exception())));
-	}
+        return (ExceptionUtils.wrapExceptionResult(r.origin().getClass().toString(),
+                unwrap(r.exception()).getMessage(), getStacktrace(r.exception()), getOriginTrace(),
+                getJsException(r.exception())));
+    }
 
-	private final static JavaScriptObject getJsException(Throwable ex) {
-		Throwable uw = unwrap(ex);
-		if (!(uw instanceof JavaScriptException)) {
-			return null;
-		}
-		
-		return ((JavaScriptException) uw).getException();
-		
-		
-	}
-	
-	public static final String getStacktrace(final Throwable r) {
-		Throwable unwrapped = unwrap(r);
-		String stacktrace;
-		try {
-			stacktrace = unwrapped.toString() + "<br />\n";
-		} catch (Throwable t) {
-			stacktrace = "Error creating stacktrace for " + r.getMessage()
-					+ ".\n  Exception reported: " + t.getMessage();
+    private final static JavaScriptObject getJsException(final Throwable ex) {
+        final Throwable uw = unwrap(ex);
+        if (!(uw instanceof JavaScriptException)) {
+            return null;
+        }
 
-		}
+        return ((JavaScriptException) uw).getException();
 
-		if (unwrapped instanceof JavaScriptException) {
-			JavaScriptException jsException = (JavaScriptException) unwrapped;
-			if (jsException.getException() != null) {
-				stacktrace += "JavaScriptException:<br/>\n"
-						+ getJavaScriptExceptionStackTrace(
-								jsException.getException()).replaceAll("\n",
-								"<br/>\n")
-						+ "<br/>\n-- End of JavaScriptException";
-			}
+    }
 
-		}
+    public static final String getStacktrace(final Throwable r) {
+        if (r == null) {
+            throw new IllegalArgumentException("Cannot create stacktrace for null.");
+        }
+        final Throwable unwrapped = unwrap(r);
+        String stacktrace;
+        try {
+            stacktrace = unwrapped.toString() + "<br />\n";
+        } catch (final Throwable t) {
+            stacktrace = "Error creating stacktrace for " + r.getMessage() + ".\n  Exception reported: "
+                    + t.getMessage();
 
-		for (final StackTraceElement element : unwrapped.getStackTrace()) {
-			stacktrace += element + "<br/>\n";
-		}
+        }
 
-		stacktrace += getCauseTrace(unwrap(r));
-		return stacktrace;
-	}
+        if (unwrapped instanceof JavaScriptException) {
+            final JavaScriptException jsException = (JavaScriptException) unwrapped;
+            if (jsException.getException() != null) {
+                stacktrace += "JavaScriptException:<br/>\n"
+                        + getJavaScriptExceptionStackTrace(jsException.getException()).replaceAll("\n", "<br/>\n")
+                        + "<br/>\n-- End of JavaScriptException";
+            }
 
-	private static native final String getJavaScriptExceptionStackTrace(
-			JavaScriptObject ex)/*-{
-								if (!ex.stack) {
-								return "JavaScriptException stack trace not available for this browser";
-								}
-								return ex.stack;
-								}-*/;
+        }
 
-	private static final String getOriginTrace() {
-		try {
-			throw new Exception("Origin");
-		} catch (Throwable t) {
-			return getStacktrace(t);
-		}
+        for (final StackTraceElement element : unwrapped.getStackTrace()) {
+            stacktrace += element + "<br/>\n";
+        }
 
-	}
+        stacktrace += getCauseTrace(unwrap(r));
+        return stacktrace;
+    }
 
-	private static final Throwable unwrap(final Throwable e) {
-		if (e instanceof UmbrellaException) {
-			final UmbrellaException ue = (UmbrellaException) e;
-			if (ue.getCauses().size() == 1) {
-				return unwrap(ue.getCauses().iterator().next());
-			}
-		}
-		return e;
-	}
+    private static native final String getJavaScriptExceptionStackTrace(JavaScriptObject ex)/*-{
+                                                                                            if (!ex.stack) {
+                                                                                            return "JavaScriptException stack trace not available for this browser";
+                                                                                            }
+                                                                                            return ex.stack;
+                                                                                            }-*/;
 
-	private static final String getCauseTrace(final Throwable t) {
-		if (t.getCause() == null) {
-			return "-end of stack trace";
-		}
+    private static final String getOriginTrace() {
+        try {
+            throw new Exception("Origin");
+        } catch (final Throwable t) {
+            return getStacktrace(t);
+        }
 
-		String res = "Caused By: " + unwrap(t.getCause()).toString()
-				+ "<br/>\n";
-		for (final StackTraceElement element : unwrap(t.getCause())
-				.getStackTrace()) {
-			res += element + "<br/>\n";
-		}
+    }
 
-		return res;
-	}
+    private static final Throwable unwrap(final Throwable e) {
+        if (e instanceof UmbrellaException) {
+            final UmbrellaException ue = (UmbrellaException) e;
+            if (ue.getCauses().size() == 1) {
+                return unwrap(ue.getCauses().iterator().next());
+            }
+        }
+        return e;
+    }
 
-	private static final native JavaScriptObject triggerFailureCallbackJs(
-			JavaScriptObject callback, String origin, String exceptionMessage,
-			String stacktrace, String originTrace, JavaScriptObject jsException)/*-{
-													callback({
-													exception: exceptionMessage,
-													origin: origin,
-													origintrace: originTrace,
-													stacktrace: stacktrace,
-													jsException: jsException
-													});
-													}-*/;
+    private static final String getCauseTrace(final Throwable t) {
+        if (t.getCause() == null) {
+            return "-end of stack trace";
+        }
 
-	private static final native JavaScriptObject wrapExceptionResult(
-			String origin, String exceptionMessage, String stacktrace,
-			String originTrace, JavaScriptObject jsException)/*-{
-								return {
-								exception: exceptionMessage,
-								origin: origin,
-								origintrace: originTrace,
-								stacktrace: stacktrace,
-								jsException: jsException
-								}
-								}-*/;
+        String res = "Caused By: " + unwrap(t.getCause()).toString() + "<br/>\n";
+        for (final StackTraceElement element : unwrap(t.getCause()).getStackTrace()) {
+            res += element + "<br/>\n";
+        }
+
+        return res;
+    }
+
+    private static final native JavaScriptObject triggerFailureCallbackJs(JavaScriptObject callback, String origin,
+            String exceptionMessage, String stacktrace, String originTrace, JavaScriptObject jsException)/*-{
+                                                                                                         callback({
+                                                                                                         exception: exceptionMessage,
+                                                                                                         origin: origin,
+                                                                                                         origintrace: originTrace,
+                                                                                                         stacktrace: stacktrace,
+                                                                                                         jsException: jsException
+                                                                                                         });
+                                                                                                         }-*/;
+
+    private static final native JavaScriptObject wrapExceptionResult(String origin, String exceptionMessage,
+            String stacktrace, String originTrace, JavaScriptObject jsException)/*-{
+                                                                                return {
+                                                                                exception: exceptionMessage,
+                                                                                origin: origin,
+                                                                                origintrace: originTrace,
+                                                                                stacktrace: stacktrace,
+                                                                                jsException: jsException
+                                                                                }
+                                                                                }-*/;
 
 }
