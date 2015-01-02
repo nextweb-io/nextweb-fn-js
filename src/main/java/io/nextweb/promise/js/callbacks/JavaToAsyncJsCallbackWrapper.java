7ad89db30142e9faf48c4e838ce2aae9d677526a
mxro <mxro@nowhere.com>
2015-01-02 11:55:23 +1100

    Making interoperability with Async.js easier Autocommit Fri Jan 02 11:55:23 EST 2015

--- src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsCallbackWrapper.java
+++ src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsCallbackWrapper.java
@@ -2,12 +2,23 @@ package io.nextweb.promise.js.callbacks;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+import de.mxro.async.callbacks.ValueCallback;
+
 public class JavaToAsyncJsCallbackWrapper {
 
-    private native final JavaScriptObject createCb()/*-{
-                                                    return function(ex, result) {
-                                                    
-                                                    };
-                                                    }-*/;
+    public static JavaScriptObject wrap(final ValueCallback<Object> cb) {
+
+    }
+
+    private native static final JavaScriptObject createCb(JavaScriptObject cb)/*-{
+                                                                              return function(ex, result) {
+                                                                              if (ex) {
+                                                                              cb.onFailure(ex);
+                                                                              return;
+                                                                              }
+                                                                              
+                                                                              cb.onSuccess(result);
+                                                                              };
+                                                                              }-*/;
 
 }
