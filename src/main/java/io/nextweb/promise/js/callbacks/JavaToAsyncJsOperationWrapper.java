0b30eb36f32f26616a04e6e0ae9ae1d1da69051e
mxro <mxro@nowhere.com>
2015-01-02 12:08:23 +1100

    Making interoperability with Async.js easier Autocommit Fri Jan 02 12:08:23 EST 2015

--- src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsOperationWrapper.java
+++ src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsOperationWrapper.java
@@ -1,24 +1,42 @@
 package io.nextweb.promise.js.callbacks;
 
-import org.timepedia.exporter.client.ExporterUtil;
-
+import io.nextweb.promise.NextwebPromise;
+import io.nextweb.promise.exceptions.ExceptionListener;
+import io.nextweb.promise.exceptions.ExceptionResult;
 import io.nextweb.promise.js.JsClosure;
+
+import org.timepedia.exporter.client.ExporterUtil;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
-import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.fn.Closure;
 
 public class JavaToAsyncJsOperationWrapper {
 
-    public static JavaScriptObject wrap(final ValueCallback<Object> cb) {
+    public static <V> JavaScriptObject wrap(final NextwebPromise<V> promise) {
         final JavaScriptObject operation = ExporterUtil.wrap(new JsClosure() {
 
             @Override
             public void apply(final Object result) {
-                   AsyncJsToJavaCallbackWrapper callback = new AsyncJsToJavaCallbackWrapper(ExporterUtil.wrap(result));
-                   
-                   
-                   
+                final AsyncJsToJavaCallbackWrapper callback = new AsyncJsToJavaCallbackWrapper(ExporterUtil
+                        .wrap(result));
+
+                promise.catchExceptions(new ExceptionListener() {
+
+                    @Override
+                    public void onFailure(final ExceptionResult r) {
+                        callback.onFailure(r.exception());
+                    }
+                });
+
+                promise.get(new Closure<V>() {
+
+                    @Override
+                    public void apply(final V o) {
+                        callback.onSuccess(o);
+                    }
+                });
+
             }
         });
 
