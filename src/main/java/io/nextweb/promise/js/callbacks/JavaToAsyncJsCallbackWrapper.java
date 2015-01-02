d61e2a10c039ebff23f4703695abbb696045c16e
mxro <mxro@nowhere.com>
2015-01-02 12:02:23 +1100

    Making interoperability with Async.js easier Autocommit Fri Jan 02 12:02:23 EST 2015

--- src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsCallbackWrapper.java
+++ src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsCallbackWrapper.java
@@ -1,5 +1,7 @@
 package io.nextweb.promise.js.callbacks;
 
+import io.nextweb.promise.js.JsClosure;
+
 import com.google.gwt.core.client.JavaScriptObject;
 
 import de.mxro.async.callbacks.ValueCallback;
@@ -7,20 +9,21 @@ import de.mxro.async.callbacks.ValueCallback;
 public class JavaToAsyncJsCallbackWrapper {
 
     public static JavaScriptObject wrap(final ValueCallback<Object> cb) {
-        final JavaScriptObject jsValueCallback = JsValueCallbackWrapper.wrap(cb);
+        final JavaScriptObject operation = new JsClosure() {
+
+            @Override
+            public void apply(final Object result) {
+
+            }
+        };
 
         return createCb(jsValueCallback);
     }
 
-    private native static final JavaScriptObject createCb(JavaScriptObject cb)/*-{
-                                                                              return function(ex, result) {
-                                                                              if (ex) {
-                                                                              cb.onFailure(ex);
-                                                                              return;
-                                                                              }
-
-                                                                              cb.onSuccess(result);
-                                                                              };
-                                                                              }-*/;
+    private native static final JavaScriptObject createCb(JavaScriptObject operation)/*-{
+                                                                                     return function(cb) {
+                                                                                     operation.apply(cb);
+                                                                                     };
+                                                                                     }-*/;
 
 }
