3aeccebc604e8dcf4fb9276a2d0b03e51dabd006
mxro <mxro@nowhere.com>
2015-01-02 12:27:23 +1100

    Making interoperability with Async.js easier Autocommit Fri Jan 02 12:27:23 EST 2015

--- src/main/java/io/nextweb/promise/js/JsNextwebPromise.java
+++ src/main/java/io/nextweb/promise/js/JsNextwebPromise.java
@@ -27,7 +27,7 @@ import de.mxro.fn.Closure;
  */
 @Export
 public class JsNextwebPromise<T, R extends BasicPromise<T>> implements Exportable,
-JsBasicPromise<JsNextwebPromise<T, R>>, JsWrapper<R> {
+        JsBasicPromise<JsNextwebPromise<T, R>>, JsWrapper<R> {
 
     R result;
     WrapperCollection wrappers;
