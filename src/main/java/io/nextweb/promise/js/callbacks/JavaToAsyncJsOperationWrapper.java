9689fdfba18df03f05667ac0292b89886323653a
mxro <mxro@nowhere.com>
2015-01-02 12:04:23 +1100

    Making interoperability with Async.js easier Autocommit Fri Jan 02 12:04:23 EST 2015

--- src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsOperationWrapper.java
+++ src/main/java/io/nextweb/promise/js/callbacks/JavaToAsyncJsOperationWrapper.java
@@ -1,23 +1,28 @@
 package io.nextweb.promise.js.callbacks;
 
+import org.timepedia.exporter.client.ExporterUtil;
+
 import io.nextweb.promise.js.JsClosure;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
 import de.mxro.async.callbacks.ValueCallback;
 
-public class JavaToAsyncJsCallbackWrapper {
+public class JavaToAsyncJsOperationWrapper {
 
     public static JavaScriptObject wrap(final ValueCallback<Object> cb) {
-        final JavaScriptObject operation = new JsClosure() {
+        final JavaScriptObject operation = ExporterUtil.wrap(new JsClosure() {
 
             @Override
             public void apply(final Object result) {
-
+                   AsyncJsToJavaCallbackWrapper callback = new AsyncJsToJavaCallbackWrapper(ExporterUtil.wrap(result));
+                   
+                   
+                   
             }
-        };
+        });
 
-        return createCb(jsValueCallback);
+        return createCb(operation);
     }
 
     private native static final JavaScriptObject createCb(JavaScriptObject operation)/*-{
