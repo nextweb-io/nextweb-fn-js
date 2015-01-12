082352f17f980b7f30a7a13fb87f912ed559df3d
mxro <mxro@nowhere.com>
2015-01-13 07:56:26 +1100

    Simplifiying documentation Autocommit Tue Jan 13 07:56:26 EST 2015

--- src/main/java/io/nextweb/promise/js/Console.java
+++ src/main/java/io/nextweb/promise/js/Console.java
@@ -1,12 +1,14 @@
 package io.nextweb.promise.js;
 
+import com.google.gwt.core.client.JavaScriptObject;
+
 public final class Console {
 
     /**
      * <p>
      * Log a message to the browser console if available.
      * <p>
-     * Otherwise, show an alert.
+     * Otherwise, do nothing.
      * 
      * @param message
      *            The message to be displayed.
@@ -15,14 +17,16 @@ public final class Console {
         logNative(message);
     }
 
+    public static final void log(final JavaScriptObject object) {
+        logNativeObject(message);
+    }
+
     private static final native void logNative(String message)/*-{
                                                               if (window.console) { 
 
                                                               window.console.log(message);
                                                               return;
                                                               }
-
-                                                              window.alert(message);
 
                                                               }-*/;
 
