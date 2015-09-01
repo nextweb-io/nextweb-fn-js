9a5f9a215d20bb53d66e2476faf2355fc14c2dbd
mxro <mxro@nowhere.com>
2015-09-01 17:34:08 +1000

    a

---
+++ src/main/java/io/nextweb/promise/js/JsMaybe.java
@@ -0,0 +1,39 @@
+package io.nextweb.promise.js;
+
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.Exportable;
+import org.timepedia.exporter.client.NoExport;
+
+@Export
+public class JsMaybe implements Exportable {
+
+    private Object value;
+
+    @Export
+    public Object is() {
+        if (value != null) {
+            return "is";
+        } else {
+            return null;
+        }
+    }
+
+    @Export
+    public Object value() {
+        return value;
+    }
+
+    @NoExport
+    public JsMaybe fromValue(final Object value) {
+        final JsMaybe maybe = new JsMaybe();
+
+        maybe.value = value;
+
+        return maybe;
+    }
+
+    public JsMaybe() {
+        super();
+    }
+
+}
