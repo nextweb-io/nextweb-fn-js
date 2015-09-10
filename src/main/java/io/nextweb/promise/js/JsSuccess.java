a48cf545f116cecb87b908e3a31b5b224bfe961b
mxro <mxro@nowhere.com>
2015-09-11 08:02:57 +1000

    Develop JS API for Hooks Autocommit Fri Sep 11 08:02:57 EST 2015

--- src/main/java/io/nextweb/promise/js/JsSuccess.java
+++ src/main/java/io/nextweb/promise/js/JsSuccess.java
@@ -1,18 +1,39 @@
 package io.nextweb.promise.js;
 
+import delight.functional.Function;
+import delight.functional.Success;
+
 import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.Exportable;
+import org.timepedia.exporter.client.NoExport;
 
 @Export
 public class JsSuccess implements Exportable {
 
-	@Export
-	public void isSuccess() {
+    @Export
+    public void isSuccess() {
 
-	}
+    }
 
-	public JsSuccess() {
-		super();
-	}
+    public JsSuccess() {
+        super();
+    }
+
+    @NoExport
+    public static Function<Object, Object> getWrapper() {
+        return new Function<Object, Object>() {
+
+            @Override
+            public Object apply(final Object input) {
+
+                if (!(input instanceof Success)) {
+                    throw new IllegalArgumentException(
+                            "Result of type Success expected. Instead received: " + input.getClass());
+                }
+
+                return new JsSuccess();
+            }
+        };
+    }
 
 }
