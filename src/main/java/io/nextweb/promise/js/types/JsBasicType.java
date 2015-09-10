86e33c29bb321093c1bc880ab0128a24d7fd3280
mxro <mxro@nowhere.com>
2015-09-11 09:55:58 +1000

    Develop JS API for Hooks Autocommit Fri Sep 11 09:55:58 EST 2015

--- src/main/java/io/nextweb/promise/js/types/JsBasicType.java
+++ src/main/java/io/nextweb/promise/js/types/JsBasicType.java
@@ -10,95 +10,95 @@ import io.nextweb.promise.js.FnJs;
 @Export
 public class JsBasicType implements Exportable {
 
-	private Object value;
+    private Object value;
 
-	@Export
-	public int intValue() {
+    @Export
+    public int intValue() {
 
-		return ((Number) value).intValue();
-	}
+        return ((Number) value).intValue();
+    }
 
-	@Export
-	public String stringValue() {
-		return (String) value;
-	}
+    @Export
+    public String stringValue() {
+        return (String) value;
+    }
 
-	@Export
-	public JavaScriptObject booleanValue() {
-		if (((Boolean) value).booleanValue() == true) {
-			return booleanTrue();
-		} else {
-			return booleanFalse();
-		}
-	}
+    @Export
+    public JavaScriptObject booleanValue() {
+        if (((Boolean) value).booleanValue() == true) {
+            return booleanTrue();
+        } else {
+            return booleanFalse();
+        }
+    }
 
-	private final native JavaScriptObject booleanTrue()/*-{
-														var val = {
-														value : new Boolean(true)
-														};
-														return val;
-														}-*/;
+    private final native JavaScriptObject booleanTrue()/*-{
+                                                       var val = {
+                                                       value : new Boolean(true)
+                                                       };
+                                                       return val;
+                                                       }-*/;
 
-	private final native JavaScriptObject booleanFalse()/*-{
-														var val = {
-														value : new Boolean(false)
-														};
-														return val;
-														}-*/;
+    private final native JavaScriptObject booleanFalse()/*-{
+                                                        var val = {
+                                                        value : new Boolean(false)
+                                                        };
+                                                        return val;
+                                                        }-*/;
 
-	@Export
-	public double doubleValue() {
+    @Export
+    public double doubleValue() {
 
-		return (Double) value;
-	}
+        return (Double) value;
+    }
 
-	@Export
-	public int isString() {
-		return FnJs.isJsString(value) ? 1 : 0;
-	}
+    @Export
+    public int isString() {
+        return FnJs.isJsString(value) ? 1 : 0;
+    }
 
-	@Export
-	public int isInt() {
-		return FnJs.isJsInteger(value) ? 1 : 0;
-	}
+    @Export
+    public int isInt() {
+        return FnJs.isJsInteger(value) ? 1 : 0;
+    }
 
-	@Export
-	public int isDouble() {
-		return FnJs.isJsDouble(value) ? 1 : 0;
-	}
+    @Export
+    public int isDouble() {
+        return FnJs.isJsDouble(value) ? 1 : 0;
+    }
 
-	@Export
-	public int isBoolean() {
-		return value instanceof Boolean ? 1 : 0;
-	}
+    @Export
+    public int isBoolean() {
+        return value instanceof Boolean ? 1 : 0;
+    }
 
-	@Export
-	public int isJsBasicType() {
-		return 1;
-	}
+    @Export
+    public int isJsBasicType() {
+        return 1;
+    }
 
-	@Export
-	@Override
-	public String toString() {
-		return "wrapped[" + value + ", " + value.getClass() + "]";
-	}
+    @Export
+    @Override
+    public String toString() {
+        return "wrapped[" + value + ", " + value.getClass() + "]";
+    }
 
-	public void setValue(final Object value) {
-		this.value = value;
-	}
+    public void setValue(final Object value) {
+        this.value = value;
+    }
 
-	public static JsBasicType wrap(final Object node) {
-		final JsBasicType jsBasicType = new JsBasicType();
+    public static JsBasicType wrap(final Object node) {
+        final JsBasicType jsBasicType = new JsBasicType();
 
-		jsBasicType.setValue(node);
-		assert jsBasicType.isString() != 0 || jsBasicType.isInt() != 0
-				|| jsBasicType.isDouble() != 0 || jsBasicType.isBoolean() != 0 : "Attempting to wrap non-basic type.";
+        jsBasicType.setValue(node);
+        assert jsBasicType.isString() != 0 || jsBasicType.isInt() != 0 || jsBasicType.isDouble() != 0
+                || jsBasicType.isBoolean() != 0 : "Attempting to wrap non-basic type.";
 
-		return jsBasicType;
-	}
+        return jsBasicType;
+    }
 
-	public JsBasicType() {
-		super();
-	}
+    public JsBasicType() {
+        super();
+    }
 
 }
