132637296022260c2d3728056118bdf702130e76
mxro <mxro@nowhere.com>
2015-10-28 10:54:41 +1100

    Implementing ObjectHub UI Autocommit Wed Oct 28 10:54:41 EST 2015

--- src/main/java/io/nextweb/promise/js/JsDataPromise.java
+++ src/main/java/io/nextweb/promise/js/JsDataPromise.java
@@ -3,6 +3,7 @@ package io.nextweb.promise.js;
 import delight.async.callbacks.ValueCallback;
 import delight.functional.Closure;
 import delight.functional.Function;
+import delight.gwt.console.Console;
 
 import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.Exportable;
@@ -54,6 +55,7 @@ public class JsDataPromise<T, R extends BasicPromise<T>>
 
             @Override
             public void onFailure(final ExceptionResult r) {
+                Console.log("Excpetion manager received: " + r);
                 exceptionManager().getOriginal().onFailure(r);
             }
         }));
